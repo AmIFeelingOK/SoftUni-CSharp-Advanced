02bd4fb1d87dd6d05352d40b377747f13dcb9e17
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-17 23:06:38 +0300

    Add files via upload

---
+++ 01.StacksAndQueues-Lab/04.MatchingBrackets/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expr = Console.ReadLine();
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < expr.Length; i++)
+            {
+                char chr = expr[i];
+
+                if (chr == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (chr == ')')
+                {
+                    int startIndex = stack.Pop();
+                    int endIndex = i;
+                    string subExpr = expr.Substring(startIndex, endIndex - startIndex + 1);
+
+                    Console.WriteLine(subExpr);
+                }
+            }
+        }
+    }
+}
