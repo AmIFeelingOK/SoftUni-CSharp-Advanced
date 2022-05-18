c2999e3da36149e313fc007ba1e72c0207b792d9
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-19 02:51:19 +0300

    Add files via upload

---
+++ 01.StacksAndQueues-Lab/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05.PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            Queue<int> inputStack = new Queue<int>(input);
+
+            int counter = inputStack.Count;
+
+            for (int i = 0; i < counter; i++)
+            {
+                if (inputStack.Peek() % 2 == 1)
+                    inputStack.Dequeue();
+                else
+                    inputStack.Enqueue(inputStack.Dequeue());
+            }
+
+            Console.WriteLine(string.Join(", ", inputStack));
+        }
+    }
+}
