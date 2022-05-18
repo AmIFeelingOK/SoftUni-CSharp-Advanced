e8c4df0e8bcebebcc45c532b9dc7b18a22fafa49
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-18 17:47:37 +0300

    Add files via upload

---
+++ 01.StacksAndQueues-Lab/03.SimpleCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _3.SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> input = new Stack<string>(Console.ReadLine().Split().Reverse());
+
+            while (input.Count > 1)
+            {
+                int a = int.Parse(input.Pop());
+
+                string symbol = input.Pop();
+
+                int b = int.Parse(input.Pop());
+
+                if (symbol == "+")
+                {
+                    input.Push((a + b).ToString());
+                }
+                else if (symbol == "-")
+                {
+                    input.Push((a - b).ToString());
+                }
+            }
+
+            Console.WriteLine(input.Pop());
+        }
+    }
+}
