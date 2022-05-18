78fe211d9859c67761dd44a78bd8cb2659408a59
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-19 02:53:16 +0300

    Add files via upload

---
+++ 01.StacksAndQueues-Lab/08.TrafficJam/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> queue = new Queue<string>();
+
+            int carsPassed = 0;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "green")
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (queue.Count > 0)
+                        {
+                            string car = queue.Dequeue();
+                            Console.WriteLine($"{car} passed!");
+                            carsPassed++;
+                        }
+                    }
+
+                }
+                else if (command == "end")
+                {
+                    Console.WriteLine($"{carsPassed} passed the crossroads.");
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+            }
+        }
+    }
+}
