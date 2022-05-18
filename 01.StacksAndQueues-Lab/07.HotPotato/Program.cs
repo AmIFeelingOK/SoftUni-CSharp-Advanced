e1e32a98106629e727053983737a4df7a98ec8f5
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-19 02:52:51 +0300

    Add files via upload

---
+++ 01.StacksAndQueues-Lab/07.HotPotato/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _7.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] players = Console.ReadLine().Split().ToArray();
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> queue = new Queue<string>(players);
+
+            while (queue.Count > 1)
+            {
+                for (int i = 1; i <= n - 1; i++)
+                {
+                    string player = queue.Dequeue();
+                    queue.Enqueue(player);
+                }
+
+                string lostPlayer = queue.Dequeue();
+                Console.WriteLine($"Removed {lostPlayer}");
+            }
+
+            string lastPlayer = queue.Dequeue();
+            Console.WriteLine($"Last is {lastPlayer}");
+        }
+    }
+}
