fc9d4274be340eddf5bcec3a37a31e0e0abd7e2b
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-19 02:52:04 +0300

    Add files via upload

---
+++ 01.StacksAndQueues-Lab/06.Supermarket/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            Queue<string> queue = new Queue<string>();
+
+            while (command != "End")
+            {
+                if (command == "Paid")
+                {
+                    foreach (var customer in queue)
+                    {
+                        Console.WriteLine(customer);
+                    }
+
+                    queue.Clear();
+                    command = Console.ReadLine();
+                    continue;
+                }
+                
+                queue.Enqueue(command);
+                command = Console.ReadLine();                
+            }
+
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
