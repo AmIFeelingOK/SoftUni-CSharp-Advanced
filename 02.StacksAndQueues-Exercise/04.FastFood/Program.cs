0acff25672f4480fbd74bebc516f071002db68b3
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-20 01:17:38 +0300

    Add files via upload

---
+++ 02.StacksAndQueues-Exercise/04.FastFood/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04.FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantity = int.Parse(Console.ReadLine());
+
+            Queue<int> queue = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            Console.WriteLine($"{queue.Max()}");
+
+            int counter = queue.Count;
+
+            for (int i = 1; i <= counter; i++)
+            {
+                if (quantity >= queue.Peek())
+                {
+                    quantity -= queue.Peek();
+                    queue.Dequeue();
+                }
+                else
+                    break;
+            }
+
+            if (!queue.Any())
+            {
+                Console.WriteLine($"Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: " + string.Join(" ", queue));
+            }            
+        }
+    }
+}
