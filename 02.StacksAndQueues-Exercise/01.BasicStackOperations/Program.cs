7957b368814487f7369f3029695506097e750d4f
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-19 14:04:54 +0300

    Add files via upload

---
+++ 02.StacksAndQueues-Exercise/01.BasicStackOperations/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01.BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = array[0];
+            int s = array[1];
+            int x = array[2];
+
+            int[] numbers = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+                stack.Push(numbers[i]);
+
+            for (int j = 0; j < s; j++)
+                stack.Pop();
+            if (stack.Count == 0)
+                Console.WriteLine(0);
+            
+            else if (stack.Contains(x))
+                Console.WriteLine("true");
+            
+            else if (!stack.Contains(x))
+                Console.WriteLine(stack.Min());
+        }
+    }
+}
---
+++ 02.StacksAndQueues-Exercise/03.MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03.MaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split();
+
+                if (input[0] == "1")
+                    stack.Push(int.Parse(input[1]));
+
+                else if (input[0] == "2")
+                {
+                    if (stack.Any())
+                        stack.Pop();
+                }
+                else if (input[0] == "3")
+                {
+                    if (stack.Any())
+                        Console.WriteLine(stack.Max());
+                }
+                else if (input[0] == "4")
+                {
+                    if (stack.Any())
+                        Console.WriteLine(stack.Min());
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
