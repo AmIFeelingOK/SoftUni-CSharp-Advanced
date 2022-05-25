de12a4f849c1ad33eace92fc32eb64f81d056b8c
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-25 18:00:56 +0300

    Add files via upload

---
+++ 05.SetsAndDictionariesAdvanced-Lab/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountSameValuesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = new Dictionary<double, int>();
+
+            double[] arrayOfNumbers = Console.ReadLine()
+                .Split(" ")
+                .Select(double.Parse)
+                .ToArray();
+
+            foreach (var number in arrayOfNumbers)
+            {
+                if (counter.ContainsKey(number))
+                    counter[number]++;
+                else
+                    counter[number] = 1;
+            }
+
+            foreach (var pair in counter)
+            {
+                Console.WriteLine($"{pair.Key} - {pair.Value} times");
+            }
+        }
+    }
+}
---
+++ 05.SetsAndDictionariesAdvanced-Lab/02.AverageStudentGrades/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, List<decimal>>();
+
+            int numberOfStudents = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                string[] information = Console.ReadLine().Split();
+
+                string name = information[0];
+                decimal grade = decimal.Parse(information[1]);
+
+                if (!dictionary.ContainsKey(name))
+                    dictionary.Add(name, new List<decimal>());
+
+                dictionary[name].Add(grade);
+            }
+
+            foreach (var name in dictionary.Keys)
+            {
+                List<decimal> grades = dictionary[name];
+                string gradeStr = string.Join(" ", grades.Select(x => x.ToString("f2")));
+
+                decimal average = grades.Average();
+
+                Console.WriteLine($"{name} -> {gradeStr} (avg: {average:f2})");
+            }
+        }
+    }
+}
