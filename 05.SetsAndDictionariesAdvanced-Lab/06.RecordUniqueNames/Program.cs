fce8b9f0c1b59d6187832157ad8a12e03ba6ecf6
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-27 21:49:02 +0300

    Add files via upload

---
+++ 05.SetsAndDictionariesAdvanced-Lab/06.RecordUniqueNames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.RecordUniqueNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> namesCollection = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string names = Console.ReadLine();
+
+                namesCollection.Add(names);
+            }
+
+            foreach (string name in namesCollection)
+                Console.WriteLine(name);
+        }
+    }
+}
