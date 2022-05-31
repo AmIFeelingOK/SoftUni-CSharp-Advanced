32277d8a0390462d8c58faa728391ff1b5680ec1
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-31 23:46:39 +0300

    Add files via upload

---
+++ 05.SetsAndDictionariesAdvanced-Lab/07.ParkingLot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> cars = new HashSet<string>();
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] argCmd = command.Split(", ");
+
+                if (argCmd[0] == "IN")
+                    cars.Add(argCmd[1]);
+                else if (argCmd[0] == "OUT")
+                    cars.Remove(argCmd[1]);
+
+                command = Console.ReadLine();
+            }
+
+            if (cars.Count <= 0)
+                Console.WriteLine("Parking Lot is Empty");
+            else
+                Console.WriteLine(string.Join("\n", cars));
+        }
+    }
+}
---
+++ 05.SetsAndDictionariesAdvanced-Lab/08.SoftUniParty/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> guests = new HashSet<string>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "PARTY")
+                    break;
+
+                guests.Add(command);
+            }
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "END")
+                    break;
+
+                if (guests.Contains(command))
+                    guests.Remove(command);
+            }
+
+            Console.WriteLine(guests.Count);
+
+            foreach (var guest in guests)
+                if (char.IsDigit(guest[0]))
+                    Console.WriteLine(guest);
+
+            foreach (var guest in guests)
+                if (char.IsLetter(guest[0]))
+                    Console.WriteLine(guest);
+        }
+    }
+}
