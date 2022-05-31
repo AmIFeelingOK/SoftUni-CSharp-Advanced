6e0996819a1661e52938aa791dd418214f2324d2
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-31 12:12:28 +0300

    Add files via upload

---
+++ 09.FunctionalProgramming-Lab/02.SumNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(", ")
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", numbers.Length));
+            Console.WriteLine(string.Join(" ", numbers.Sum()));
+        }
+    }
+}
