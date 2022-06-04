598283f63628d569243aa71f2d3621bb3250508f
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-06-04 22:11:29 +0300

    Add files via upload

---
+++ 03.MultidimensionalArrays-Lab/01.SumMatrixElements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(", ")
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            (int rows, int cols) = (nums[0], nums[1]);
+
+            int[,] matrix = new int [rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] command = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = command[col];
+                }
+            }
+
+            long sum = 0;
+
+            for (int row = 0; row < rows; row++)
+                for (int col = 0; col < cols; col++)
+                    sum += matrix[row, col];
+
+            Console.WriteLine($"{rows}\n{cols}\n{sum}");
+        }
+    }
+}
---
+++ 03.MultidimensionalArrays-Lab/02.SumMatrixColumns/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(", ")
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            (int rows, int cols) = (nums[0], nums[1]);
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] command = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = command[col];
+                }
+            }
+
+            long[] colSum = new long[cols];
+
+            for (int i = 0; i < cols; i++)
+            {
+                long sum = 0;
+
+                for (int j = 0; j < rows; j++)
+                {
+                    sum += matrix[j, i];
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
