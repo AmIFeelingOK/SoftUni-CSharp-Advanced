79d02891b80ec8cef6a057ff2fada62ce4701beb
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-06-07 11:54:59 +0300

    Add files via upload

---
+++ 04.MultidimensionalArrays-Exercise/02.SquaresInMatrix/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split();
+
+            int rows = int.Parse(input[0]);
+            int cols = int.Parse(input[1]);
+
+            string[,] matrix = new string[rows, cols];
+
+            int counter = 0;
+
+            FillMatrix(matrix);
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1]
+                        && matrix[row, col] == matrix[row + 1, col]
+                        && matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+        private static void FillMatrix(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] rowData = Console.ReadLine()
+                    .Split();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+        }
+    }
+}
