fbe0019fbc36150dd70f02e5ebee1dd02e58380d
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-06-10 09:44:17 +0300

    Add files via upload

---
+++ 04.MultidimensionalArrays-Exercise/08.Bombs/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbers[col];
+                }
+            }
+
+            string[] bombs = Console.ReadLine().Split();
+
+            for (int i = 0; i < bombs.Length; i++)
+            {
+                string[] currBombcoordinates = bombs[i].Split(",");
+                int rowIndex = int.Parse(currBombcoordinates[0]);
+                int colIndex = int.Parse(currBombcoordinates[1]);
+                int bombing = matrix[rowIndex, colIndex];
+
+                if (bombing > 0)
+                {
+                    
+                    if (Exist(matrix, rowIndex - 1, colIndex - 1) && matrix[rowIndex - 1, colIndex - 1] > 0 &&
+                        matrix[rowIndex, colIndex] > 0)
+                    {
+                        matrix[rowIndex - 1, colIndex - 1] -= matrix[rowIndex, colIndex];
+                    }
+                    
+                    if (Exist(matrix, rowIndex - 1, colIndex) && matrix[rowIndex - 1, colIndex] > 0 &&
+                        matrix[rowIndex, colIndex] > 0)
+                    {
+                        matrix[rowIndex - 1, colIndex] -= matrix[rowIndex, colIndex];
+                    }
+                    
+                    if (Exist(matrix, rowIndex - 1, colIndex + 1) && matrix[rowIndex - 1, colIndex + 1] > 0 &&
+                        matrix[rowIndex, colIndex] > 0)
+                    {
+                        matrix[rowIndex - 1, colIndex + 1] -= matrix[rowIndex, colIndex];
+                    }
+                    
+                    if (Exist(matrix, rowIndex, colIndex + 1) && matrix[rowIndex, colIndex + 1] > 0 &&
+                        matrix[rowIndex, colIndex] > 0)
+                    {
+                        matrix[rowIndex, colIndex + 1] -= matrix[rowIndex, colIndex];
+                    }
+                    
+                    if (Exist(matrix, rowIndex + 1, colIndex + 1) && matrix[rowIndex + 1, colIndex + 1] > 0 &&
+                        matrix[rowIndex, colIndex] > 0)
+                    {
+                        matrix[rowIndex + 1, colIndex + 1] -= matrix[rowIndex, colIndex];
+                    }
+                    
+                    if (Exist(matrix, rowIndex + 1, colIndex) && matrix[rowIndex + 1, colIndex] > 0 &&
+                        matrix[rowIndex, colIndex] > 0)
+                    {
+                        matrix[rowIndex + 1, colIndex] -= matrix[rowIndex, colIndex];
+                    }
+                    
+                    if (Exist(matrix, rowIndex + 1, colIndex - 1) && matrix[rowIndex + 1, colIndex - 1] > 0 &&
+                        matrix[rowIndex, colIndex] > 0)
+                    {
+                        matrix[rowIndex + 1, colIndex - 1] -= matrix[rowIndex, colIndex];
+                    }
+                    
+                    if (Exist(matrix, rowIndex, colIndex - 1) && matrix[rowIndex, colIndex - 1] > 0 &&
+                        matrix[rowIndex, colIndex] > 0)
+                    {
+                        matrix[rowIndex, colIndex - 1] -= matrix[rowIndex, colIndex];
+                    }
+
+                    matrix[rowIndex, colIndex] = 0;
+                }
+
+            }
+
+            int counter = 0;
+            int sum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] > 0)
+                    {
+                        counter++;
+                        sum += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {counter}");
+            Console.WriteLine($"Sum: {sum}");
+
+            PrintMatrix(matrix);
+        }
+
+        private static bool Exist(int[,] matrix, int rowIndex, int colIndex)
+        {
+            return rowIndex >= 0 && rowIndex < matrix.GetLength(0) 
+                && colIndex >= 0 && colIndex < matrix.GetLength(1);
+        }
+
+        private static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void FillMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => int.Parse(x))
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+        }
+    }
+}
---
+++ 04.MultidimensionalArrays-Exercise/9.Miner/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.Miner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] mineGalleries = new char[n, n];
+
+            Queue<string> commands = new Queue<string>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries));
+
+            int countCoal = 0;
+            int minerRow = 0;
+            int minerCol = 0;
+
+            for (int row = 0; row < mineGalleries.GetLength(0); row++)
+            {
+                char[] temp = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < mineGalleries.GetLength(1); col++)
+                {
+                    mineGalleries[row, col] = temp[col];
+
+                    if (temp[col].Equals('c'))
+                    {
+                        countCoal++;
+                    }
+
+                    if (temp[col].Equals('s'))
+                    {
+                        minerRow = row;
+                        minerCol = col;
+                    }
+
+                }
+            }
+
+            int collectedCoal = 0;
+            bool isExitLoop = false;
+            bool isGameOver = false;
+
+            while ((commands.Count > 0) && (!isExitLoop))
+            {
+                int colStep = 0;
+                int rowStep = 0;
+
+                switch (commands.Dequeue().ToLower())
+                {
+                    case "left":
+                        colStep--;
+                        break;
+                    case "right":
+                        colStep++;
+                        break;
+                    case "up":
+                        rowStep--;
+                        break;
+                    case "down":
+                        rowStep++;
+                        break;
+                    default:
+                        break;
+                }
+
+                if (ValidIndex(mineGalleries.GetLength(0), mineGalleries.GetLength(1), minerRow + rowStep, minerCol + colStep))
+                {
+                    mineGalleries[minerRow, minerCol] = '*';
+                    if (mineGalleries[minerRow + rowStep, minerCol + colStep].Equals('c'))
+                    {
+                        collectedCoal++;
+                        countCoal--;
+                    }
+
+                    if (mineGalleries[minerRow + rowStep, minerCol + colStep].Equals('e'))
+                    {
+                        isGameOver = true;
+                        isExitLoop = true;
+                    }
+
+                    if (countCoal.Equals(0))
+                    {
+                        isExitLoop = true;
+                    }
+
+                    minerRow += rowStep;
+                    minerCol += colStep;
+
+                    mineGalleries[minerRow, minerCol] = 's';
+                }
+            }
+
+            if (countCoal.Equals(0))
+                Console.WriteLine($"You collected all coals! ({minerRow}, {minerCol})");
+            else if (isGameOver)
+                Console.WriteLine($"Game over! ({minerRow}, {minerCol})");
+            else
+                Console.WriteLine($"{countCoal} coals left. ({minerRow}, {minerCol})");
+        }
+
+        static bool ValidIndex(int maxRow, int maxCol, int newRow, int newCol)
+        {
+            bool result = ((newRow >= 0) && (newCol >= 0));
+            result = ((result) && (newRow < maxRow) && (newCol < maxCol));
+
+            return result;
+        }
+    }
+}
