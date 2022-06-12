4ab1125db3453112903d3114d9370480f98f7dc0
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-06-12 17:51:24 +0300

    Add files via upload

---
+++ 11.DefiningClasses-Lab/01.Car/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            {
+                car.Make = "Fiat";
+                car.Model = "Brava";
+                car.Year = 1997;
+            }
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}
---
+++ 11.DefiningClasses-Lab/02.CarExtension/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            {
+                car.Make = "Fiat";
+                car.Model = "Brava";
+                car.Year = 1997;
+                car.FuelConsumption = 200;
+                car.FuelQuantity = 200;
+                car.Drive(2000);
+
+                Console.WriteLine(car.WhoAmI());
+            }            
+        }
+    }
+}
