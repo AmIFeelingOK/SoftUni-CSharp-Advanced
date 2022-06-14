e28cbf84d5fe69b2955a50f181362c0ff093a2ba
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-06-14 21:50:14 +0300

    Add files via upload

---
+++ 11.DefiningClasses-Lab/03.CarConstructors/Car.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public Car()
+        {
+            this.make = "VW";
+            this.model = "Golf";
+            this.year = 2025;
+            this.fuelQuantity = 200;
+            this.fuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.fuelQuantity = 200;
+            this.fuelConsumption = 10;
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.fuelQuantity = fuelQuantity;
+            this.fuelConsumption = fuelConsumption;
+        }
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            double consumption = distance * FuelConsumption;
+
+            if (consumption <= fuelQuantity)
+                FuelQuantity -= consumption;
+            else
+                Console.WriteLine("Not enough fuel to perform this trip!");
+        }
+
+        public string WhoAmI()
+        {
+            string output = $"Make: {Make}\nModel: {Model}\nYear: {Year}\nFuel: {FuelQuantity}";
+
+            return output;
+        }
+    }
+}
