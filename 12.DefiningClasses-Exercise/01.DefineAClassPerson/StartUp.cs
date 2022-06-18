5ea11ae35a63ebc3985d19798076873e454035a8
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-06-18 14:32:41 +0300

    Add files via upload

---
+++ 12.DefiningClasses-Exercise/01.DefineAClassPerson/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Peter", 22);
+            Console.WriteLine($"Hello, my name is {person.Name} and I am {person.Age} years old.");
+            
+        }
+    }
+}
