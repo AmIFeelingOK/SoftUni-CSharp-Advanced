0c63df4d8b2953a9e23745e1e767531380292fff
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-06-19 23:43:50 +0300

    Add files via upload

---
+++ 12.DefiningClasses-Exercise/03.OldestFamilyMember/Family.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> FamilyMembers { get; set; }
+        public Family()
+        {
+            FamilyMembers = new List<Person>();
+        }
+
+        public void AddMember(Person person)
+        {
+            FamilyMembers.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            int maxAge = FamilyMembers.Max(x => x.Age);
+            return FamilyMembers.First(x => x.Age == maxAge);
+        }
+    }
+}
