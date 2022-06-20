bb8b2a7c3201d3bc8defe353f42ca46745d16ea5
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-06-20 10:11:52 +0300

    Add files via upload

---
+++ 12.DefiningClasses-Exercise/04.OpinionPoll/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] member = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person currentPerson = new Person(member[0], int.Parse(member[1]));
+
+                if (currentPerson.Age > 30)
+                {
+                    family.AddMember(currentPerson);
+                }
+            }
+
+            family.FamilyMembers.Where(x => x.Age > 30).OrderBy(x => x.Name).ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
