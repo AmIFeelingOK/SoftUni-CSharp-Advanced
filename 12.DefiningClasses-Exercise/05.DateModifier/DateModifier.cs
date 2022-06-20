f39b3ea812ce91360b6f1d486356995cd0749151
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-06-20 11:55:14 +0300

    Add files via upload

---
+++ 12.DefiningClasses-Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.DateModifier
+{
+    class DateModifier
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public int DayDifference()
+        {
+            TimeSpan difference = this.EndDate - this.StartDate;
+            return difference.Days;
+        }
+    }
+}
---
+++ 12.DefiningClasses-Exercise/05.DateModifier/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.DateModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateModifier datemodifier = new DateModifier();
+
+            datemodifier.StartDate = DateTime.Parse(Console.ReadLine());
+            datemodifier.EndDate = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine(Math.Abs(datemodifier.DayDifference()));
+        }
+    }
+}
